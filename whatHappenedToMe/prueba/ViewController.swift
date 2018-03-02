ef71e9de58330e684763a60ad608266b7890f4fd
sofia delgado gonzalez <sofiadelgadogonzalez@sofias-macbook-pro.local>
2018-03-01 23:15:32 -0600

    Agregué comentarios a código

--- whatHappenedToMe/prueba/ViewController.swift
+++ whatHappenedToMe/prueba/ViewController.swift
@@ -9,18 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //conexión de elementos de la vista con el controlador
     @IBOutlet weak var aoutletButton: UIButton!
     @IBOutlet weak var outletText: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //El nombre del elemento boton se modifica: "Touch me!"
         aoutletButton.setTitle("Touch me!", for: .normal)
         
     }
-
+    //conexión del controlador con la vista (accion que realiza el boton)
     @IBAction func actionoutletButton(_ sender: Any) {
+        //el elemento outletText recibe la cadena y la muestra en la vista
         outletText.text = "You are a genius "
     }
    
