571c7c7fd6a2be11351fbf512a8c6354d7dcc57e
sofia delgado gonzalez <sofiadelgadogonzalez@sofias-MacBook-Pro.local>
2018-03-08 20:42:01 -0600

    WorldTroter chapter 3

---
+++ WorldTroter/WorldTroter/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  WorldTroter
+//
+//  Created by sofia delgado gonzalez on 3/8/18.
+//  Copyright © 2018 sofia delgado gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+//    override func viewDidLoad() { //permite personalizar la jerarquia de la view
+//        //super.viewDidLoad() //Cargar la supervista a la window
+//        let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)// va ser el frame de la uiview y permite definir su tamaño y su posicion
+//        let firstView = UIView(frame: firstFrame)//agrega al constructor de uiview el frame personalizado
+//        firstView.backgroundColor = UIColor.blue //instancia de uiview en el que cambia el color de fondo a azul
+//        view.addSubview(firstView)//agrega la subvista viewcontroller
+//
+//        let secondFrame = CGRect(x: 20, y: 30, width: 50, height: 50)//creacion de otra instancia de uiview pero color verde
+//        let secondView = UIView(frame: secondFrame)
+//        secondView.backgroundColor = UIColor.green
+//        //view.addSubview(secondView) --> de viewcontroller
+//        firstView.addSubview(secondView) //depende de la vista azul
+//    }
+
+
+}
+
